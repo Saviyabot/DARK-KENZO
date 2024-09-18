17269c8126353b0b6d9680c60cbea51b5bfa84dd
KEVIN-LEVIN-TECH <kevinathenlevin@gmail.com>
2024-09-18 06:19:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "xSkUkIwD#xnHDi1lZl8GgFTlkOUQklL811mNy0igBAEN3UEGRAAo",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/GWMn14L/connet.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "Hello I am Dark-Kenzo-Md i am alive now..!",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello ${pushname} n\nI am Dark-Kenzo-Md i am alive now..!",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 SUDO_NB: process.env.SUDO_NB || "94773673969",
