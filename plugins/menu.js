02581a5b6c60e3702671e8b25f4d46a4101ab22b
╰‿╯MR HANSAMALAད★࿐ <127263909+mrhansamala@users.noreply.github.com>
2024-09-19 07:12:12 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -100,8 +100,8 @@ ${menu.fun}
 
 > 🔮𝐌𝐀𝐃𝐄 𝐁𝐘 𝐌𝐑 𝐒𝐄𝐍𝐄𝐒𝐇🔮
    `
-await conn.sendMessage(from, { audio: fs.readFileSync('./Voice/menu.mp3') , ptt: true  , mimetype: 'audio/mpeg'}, { quoted: mek })
-return await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:madeMenu},{quoted:mek})
+await conn.sendMessage(from, { audio: fs.readFileSync('./menu.mp3') , ptt: true  , mimetype: 'audio/mpeg'}, { quoted: mek })
+return await conn.sendMessage(from,{image:{url:"https://i.ibb.co/GWMn14L/connet.jpg"},caption:madeMenu},{quoted:mek})
     
 }catch(e){
 console.log(e)
